fc26b56a05d347137d4b0a5047c6d48766928262
Runys77 <rudolfslukstins@inbox.lv>
2022-10-24 16:08:23 +0300

    Add project files.

---
+++ ScooterRental/Rent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace ScooterRental
+{
+    public class Rent
+    {
+        public string Id { get; set; }
+        public decimal PricePerMinute { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        
+        public Rent(string id, decimal pricePerMinute, DateTime startTime)
+        {
+            Id = id;
+            PricePerMinute = pricePerMinute;
+            StartTime = startTime;
+        }
+    }
+}
